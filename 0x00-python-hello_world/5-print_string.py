f8a8486b7a21a74c8c6b6136f0ac0b3cbc4d6178
YourDevLebo <djlebzana@gmail.com>
2021-11-15 15:33:07 -0500

    fixed interpreter for task 2 - 8

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+split_str = str.split()
+full_str = split_str[0] + split_str[1]
+print(full_str + " " + full_str + " " + full_str + "\n" + split_str[0])
