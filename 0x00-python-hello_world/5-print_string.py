5e44a1bed508fee8ca4a8656ea0cf1059a909c71
YourDevLebo <djlebzana@gmail.com>
2021-11-16 02:24:09 -0500

    hot fix

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-split_str = str.split()
-full_str = split_str[0] + split_str[1]
-print(full_str + " " + full_str + " " + full_str + "\n" + split_str[0])
+print(3 * str)
+print(str[:9])
