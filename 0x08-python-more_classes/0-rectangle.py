06fd1da9bd2159aac5c5ddb72962cf5c1676fc22
Lebogang <33170277+YourDevLebo@users.noreply.github.com>
2022-01-08 09:18:25 +0200

    fixed documentation

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" File name : 0-rectangle.py
+    Simple rectangle : an empty class Rectangle that defines a rectangle
+    It is not allowed to import any module
+"""
+
+
 class Rectangle(object):
-    """ Define new rectangle class """
+    """Rectangle: Define new rectangle class"""
     pass
