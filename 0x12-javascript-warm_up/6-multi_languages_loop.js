3c775c6d12334cbf7e9723c82f1e456cd0d6348f
YourDevLebo <djlebzana@gmail.com>
2022-02-28 14:04:53 -0500

    fixed task 6 uppercase

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const phrases = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const phrases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while (i < phrases.length) {
   console.log(phrases[i]);
