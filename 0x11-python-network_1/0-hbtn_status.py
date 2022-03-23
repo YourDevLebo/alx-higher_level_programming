9e59244b96a86873d3685af5862ca04e8104b309
YourDevLebo <djlebzana@gmail.com>
2022-03-23 22:46:21 +0200

    fixed pep8 requirements

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,11 @@ if __name__ == '__main__':
     """ Required package """
     import urllib.request
     """ Fetching URL as reponse """
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        """ Body """
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         utf8 = html.decode('utf-8')
         print("Body response: \n\t- type: {}".format(type(html)))
-        print("\t- content: {}\n\t- utf8 content: {}".format(html,
-            utf8, end=""))
+        print("\t- content: {}\n\t- utf8 content: {}".format(
+            html, utf8, end=""))
     
